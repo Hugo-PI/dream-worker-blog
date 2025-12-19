fc6d7919d276ff8c38786dd6d2c1402404791261
Bluce Hu <blucehu@BlucedeMacBook-Pro.local>
2025-12-19 14:43:09 +0800

    修改文件

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,12 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
 // import Logo from '@/data/logo.svg'
-import Logo from '@/data/hhlogo.png';
-import Image from 'next/image';
+import Logo from '@/data/hhlogo.png'
+import Image from 'next/image'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
-
 
 const Header = () => {
   let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
@@ -21,7 +20,7 @@ const Header = () => {
         <div className="flex items-center justify-between">
           <div className="mr-3">
             {/* <Logo className="w-14 h-14"/> */}
-            <Image src={Logo} alt={siteMetadata.headerTitle} className="w-14 h-14" />
+            <Image src={Logo} alt={siteMetadata.headerTitle} className="h-14 w-14" />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
